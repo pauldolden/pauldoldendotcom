03d9ac5f214dd81ba64441f3a1a40a4e133c08b0
Paul Dolden <pauldolden@Pauls-MBP.lan>
2022-03-18 06:13:43 +0000

    FIX: bg-repeat

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,9 @@
   import '../app.css';
 </script>
 
-<main class="bg-background flex flex-col h-screen w-screen font-rubik px-10 pt-10">
+<main
+  class="bg-background flex flex-col h-screen w-screen font-rubik px-10 pt-10 bg-no-repeat bg-cover"
+>
   <slot />
   <Footer />
 </main>
