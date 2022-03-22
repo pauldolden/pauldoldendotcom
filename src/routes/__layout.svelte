b943a1205d1930f138f648f33adad0d7e9a27c50
Paul Dolden <pauldolden@Pauls-MBP.lan>
2022-03-22 05:08:42 +0000

    changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,9 @@
   import '../app.css';
 </script>
 
-<main class="flex flex-col min-h-screen min-w-screen font-rubik px-10 pt-10 slice-h md:slice-v">
+<main
+  class="flex flex-col min-h-screen min-w-screen font-rubik px-5 pt-5 md:px-10 md:pt-10 slice-h md:slice-v"
+>
   <slot />
   <Footer />
 </main>
